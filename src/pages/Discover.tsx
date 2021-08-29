c33dbca40bf5f8bc05f755ca096d4b76376df785
Terry Mun <hello@terrymun.com>
2021-08-29 04:38:04 +0200

    Avoid unnecessary new const creation

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -3,9 +3,8 @@ import LayoutContainer from '../components/LayoutContainer';
 
 /** @method */
 function User() {
-	const layoutContainerTagName = 'article';
 	return (
-		<LayoutContainer tag={layoutContainerTagName} className="py-6">
+		<LayoutContainer tag="article" className="py-6">
 			<GenericHeading level={1}>Discover <span className="text-gradient">burgers</span></GenericHeading>
 			<p>Find the next hottest burger restaurant in town. Your next burger experience awaits you:</p>
 		</LayoutContainer>
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -6,9 +6,8 @@ import { userData } from '../constants/user';
 
 /** @method */
 function Discover() {
-	const layoutContainerTagName = 'article';
 	return (
-		<LayoutContainer tag={layoutContainerTagName} className="py-6">
+		<LayoutContainer tag="article" className="py-6">
 			<GenericHeading level={1}>Hello, <span className="text-gradient">{userData.firstName}</span>!</GenericHeading>
 			<p>Manage your personal data here.</p>
 		</LayoutContainer>
